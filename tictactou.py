4155d7280dd5c5f952d3ea604c6da65c985ce068
MohammedAborezka <118435419+MohammedAborezka@users.noreply.github.com>
2024-03-02 19:57:12 +0200

    add comments and function docstring

--- tictactou.py
+++ tictactou.py
@@ -14,6 +14,19 @@ def print_sheet(sheet):
         print()
 
 def move(symbol,sheet):
+    """
+    this funcation insure that user gives us valid input and put the input in the 
+    selected place by user in the list which represent a game bord 
+    Parameters
+    ----------
+    symbol : string letter "x"or"o" represent player symbol
+    sheet :  nested list represent the game 3*3 game bord
+
+    Returns
+    -------
+    None.
+
+    """
         possible_numbers =[1,2,3]
         while True:
             try:
@@ -25,6 +38,7 @@ def move(symbol,sheet):
                 while y not in possible_numbers or x == None:
                     print("number put of range please enter vavlid number from 1 to 3")
                     y = int(input("in which column you would like to insert your symbol"))
+                    ##check if the palce already sited with symbol
                 if sheet[x-1][y-1]=="x" or sheet[x-1][y-1]=="o":
                     print("that pleace is already played")
                     continue
@@ -37,6 +51,20 @@ def move(symbol,sheet):
             
             
 def check_tie(sheet):
+    """
+    this function move on each element in the list representing the bord and
+    and check if this is empty place or filled and based on that make deccison 
+    to continue the game or return True wich represent there is a tie 
+    Parameters
+    ----------
+    sheet :  nested list represent the game 3*3 game bord
+        
+
+    Returns
+    -------
+    None.
+
+    """
     for row in sheet:
         for column in row:
             if column=="#":
@@ -45,6 +73,21 @@ def check_tie(sheet):
                 continue
     return True
 def check_winner(sheet,symbol):
+    """
+    this funcation collect the different seqeunce that can make a winner horizontal
+    or vertical or dignal in a list than check if there is 3 equally likely syboles 
+    in a row if that true it returns the symbol if no it returns False 
+    Parameters
+    ----------
+    symbol : string letter "x"or"o" represent player symbol
+    sheet :  nested list represent the game 3*3 game bord
+
+    Returns
+    -------
+    win : bool represent the status of the game starts with false changed if there
+    is a winner
+
+    """
     win = False
     elements = [row for row in sheet]
     """
@@ -92,6 +135,7 @@ sheet = [["#","#","#"],["#","#","#"],["#","#","#"]]
 players = {"x":"player1","o":"player2"}
 print("player 1 is x player 2 is o")
 while(True):
+    #check if the last turn was the last 
     if check_tie(sheet):
         print("tie")
         break
@@ -99,17 +143,20 @@ while(True):
     symbol ="x"
     print("player 1 move")
     move(symbol, sheet)
+    #check if player1 win
     if check_winner(sheet, symbol)==symbol:
         print_sheet(sheet)
         print(players[symbol]+" win")
         break
     print_sheet(sheet)
+    #check if the last turn was the last 
     if check_tie(sheet):
         print("tie")
         break
     symbol="o"
     print("player 2 move")
     move(symbol, sheet)
+    #check if player2 win 
     if check_winner(sheet, symbol)==symbol:
         print_sheet(sheet)
         print(players[symbol]+" win")
